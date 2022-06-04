9a4af47541d85911c31793e9e2da2272dfaa60ba
aanmeba <aanmeba@gmail.com>
2022-06-04 21:39:27 +1000

    implements styling modules

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-function App() {
+import Button from "./Button";
+import styles from "./App.module.css";
+
+const App = () => {
   return (
     <div>
-      <h1>Welcome back!</h1>
+      <h1 className={styles.title}>Welcome back!</h1>
+      <Button text={"Continue"} />
     </div>
   );
-}
+};
 
 export default App;

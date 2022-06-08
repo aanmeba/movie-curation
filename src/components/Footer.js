b062dd29dff9eaeeb2594eb9f98e8ec9071f83b0
aanmeba <aanmeba@gmail.com>
2022-06-08 20:17:43 +1000

    creates footer

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import { Bottom } from "./StyledComponents";
+const Footer = () => {
+  return (
+    <Bottom>
+      Powered by{" "}
+      <a
+        href="https://www.themoviedb.org/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        The Movie DB
+      </a>
+    </Bottom>
+  );
+};
+
+export default Footer;
